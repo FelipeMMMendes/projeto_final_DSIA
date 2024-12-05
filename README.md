68d2026a897bdd6eaa0f6b2024008a6a5ddcd45c
Felipe Martins <felipe_mmmachado@hotmail.com>
2024-12-05 09:36:15 -0300

    feat: acrescentado o método raíz à API e também implementado o README

---
+++ README.md
@@ -0,0 +1,44 @@
+## Projeto Final de Desenvolvimento de Sistemas de IA
+
+#### Identificação do grupo:
+
+Felipe Martins Machado Mendes - 22251506 
+Gabriel Barreto Carvalho Telles - 22251719
+Lucas Araujo de Alencar - 22251690 
+Pedro Gabriel Neves da Silva - 22251715
+
+### Como rodar o projeto:
+
+Para rodar o projeto, é preciso ter o docker instalado na máquina.
+
+1º - Clone o repositório
+
+2º - Suba o contêiner usando
+
+```
+docker compose up -d
+```
+
+3º - Após o ambiente começar a rodar, a documentação da API vai estar no endereço:
+
+[localhost:8000/docs](localhost:8000/docs)
+
+Desenvolvemos dois métodos: 
+
+- **/recomenda_produtos** - Insere o id de um produto e o sistema vai retornar outros semelhantes à ele.
+
+- **/get_products_ids** - Retorna a lista com os ids dos produtos e os seus nomes. 
+
+Estes métodos podem ser usados através da documentação ou através de seus endpoints. Recomendamos pegar os ids dos produtos primeiro para depois usá-los no outro método.
+
+Endpoints dos métodos:
+
+http://localhost:8000/get_products_ids
+
+http://localhost:8000/recomenda_produtos?product_id=B096MSW6CT
+
+Nesse exemplo acima do método de recomendação, o product id é B096MSW6CT, ele aceita apenas um product id por vez.
+
+
+
+
--- sistema_recomendacao_endpoint/api_sistema_recomendacao.py
+++ sistema_recomendacao_endpoint/api_sistema_recomendacao.py
@@ -4,14 +4,17 @@ from contextlib import asynccontextmanager
 import dill
 from pydantic import BaseModel
 
+#esse daqui é para dar carga no modelo
 def custom_load(path):
     with open(path, 'rb') as f:
         return dill.load(f)
-    
+
+#essa instanciação de classe serve para auxiliar o método get_products_ids a ter um formato de resposta   
 class Product(BaseModel):
     product_id: str
     product_name: str
 
+#esse trecho é para definir o ciclo de vida da API, de forma que o modelo do sistema de recomendação só é carregado uma vez na inicialização da API
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     global sistema_carregado
@@ -20,16 +23,29 @@ async def lifespan(app: FastAPI):
     yield
     # Aqui poderia ser colocado o código de cleanup, se necessário
 
-# Instancia a aplicação com lifespan configurado
+# Instancia a aplicação com lifespan configurado para dar carga no modelo uma vez só
 app = FastAPI(lifespan=lifespan)
 
+#metodo para a pagina inicial da API
+@app.get("/", response_model=str)
+async def root():
+    """
+    Exibe uma mensagem de boas-vindas na página inicial.
+    """
+    return "Bem-vindo à API de Recomendação de Produtos! Essa API contém dois métodos. Eles estão listados no link: localhost:8000/docs"
+
+
+#aqui definimos o método de recomendar
 @app.get("/recomenda_produtos", response_model=List[Any])
 async def recomenda_produtos(product_id: str):
     """
     Retorna os produtos recomendados.
+
+    Precisa entrar com o identificador do produto, a lista de identificadores pode ser obtida no método /get_products_ids
     """
     return sistema_carregado.recomendar(product_id)
 
+#aqui definimos o método para retornar todos os ids dos produtos
 @app.get("/get_products_ids", response_model=List[Product])
 async def get_products_ids():
     """
